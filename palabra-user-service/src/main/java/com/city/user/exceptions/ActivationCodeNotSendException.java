543ea34d54a2b8d673a61c2d8364c4dffba84adb
Gledis Gjinaj <gledisgjinaj@gmail.com>
2021-11-30 10:23:08 +0000

    user-service: imllement sms activation code service

---
+++ palabra-user-service/src/main/java/com/city/user/exceptions/ActivationCodeNotSendException.java
@@ -0,0 +1,8 @@
+package com.city.user.exceptions;
+
+public class ActivationCodeNotSendException extends RuntimeException{
+
+    public ActivationCodeNotSendException(String s) {
+        super(s);
+    }
+}
---
+++ palabra-user-service/src/main/java/com/city/user/service/SmsActivationCodeService.java
@@ -0,0 +1,44 @@
+package com.city.user.service;
+
+import com.city.palabra.config.MobileProperties;
+import com.city.user.exceptions.ActivationCodeNotSendException;
+import com.twilio.exception.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class SmsActivationCodeService implements ActivationCodeService {
+
+
+    @Autowired
+    private MobileProperties properties;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public String send(String to) {
+        try {
+            return sendSms(to);
+        } catch (ApiException e) {
+            throw new ActivationCodeNotSendException(e.getMoreInfo());
+        }
+    }
+
+    private String sendSms(String to) {
+        String code = generateCode();
+//        Twilio.init(properties.getSid(), properties.getToken());
+//        String bodyMessage = properties.getSmsText() + code;
+//        Message message = Message.creator(
+//                        new PhoneNumber(to),
+//                        new PhoneNumber(properties.getNumber()),
+//                        bodyMessage)
+//                .create();
+        log.info("@@@@@@-CODE-@@@@@: {}" ,code);
+        return encoder.encode(code);
+    }
+
+}
