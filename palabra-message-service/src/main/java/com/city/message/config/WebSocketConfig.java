0a31ef2aea0324c50252a9a0aba62cf75e8c9b4a
Gledis Gjinaj <gledisgjinaj@gmail.com>
2022-01-04 13:03:45 +0000

    Implement websocket security

--- palabra-message-service/src/main/java/com/city/message/config/WebSocketConfig.java
+++ palabra-message-service/src/main/java/com/city/message/config/WebSocketConfig.java
@@ -1,18 +1,38 @@
 package com.city.message.config;
 
+import com.city.user.exceptions.InvalidTokenException;
+import com.city.user.security.configuration.PalabraJwtAuthentication;
+import com.city.user.security.service.JwtService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private static final String AUTHORIZATION_SCHEMA = "Bearer";
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry
-                                               registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket");
     }
 
@@ -20,5 +40,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (accessor!= null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    Authentication authentication = (PalabraJwtAuthentication)accessor.getHeader("simpUser");
+                    accessor.setUser(authentication);
+                }
+                return message;
+            }
+        });
     }
 }
